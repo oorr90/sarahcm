b5618fa4814a86c965237804655810c5467514ae
Olivia Orr <oorr90@gmail.com>
2018-06-25 22:13:11 -0400

    Basic index.php page

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php get_header(); ?>
 
+<div class="main-page">
+	<div class="col col1">
+		<?php if ( have_posts() ) : ?>
+		    <?php while ( have_posts() ) : the_post(); ?>
+		        
+		    	<h1><?php the_title(); ?></h1>
 
-<?php get_sidebar(); ?>
+		    	<p><?php the_content(); ?></p>
 
+		    <?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+
+	<div class="col col2">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 
 <?php get_footer(); ?>
