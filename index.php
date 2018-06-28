c5307b682acbf7a1761dfab1e4a4a033ef2edcda
Olivia Orr <oorr90@gmail.com>
2018-06-28 19:28:09 -0400

    Adjust spacing on index page

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 	<div class="col col1">
 		<?php if ( have_posts() ) : ?>
 		    <?php while ( have_posts() ) : the_post(); ?>
-		        
-		    	<h1><?php the_title(); ?></h1>
 
-		    	<p><?php the_content(); ?></p>
+		        <div class="page-intro">
+		    		<h1><?php the_title(); ?></h1>
+
+		    		<p><?php the_content(); ?></p>
+		    	</div>
 
 		    	<?php 
 
