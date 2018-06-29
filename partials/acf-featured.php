16b928fd98eda990bb15f087de20808be0811996
Olivia Orr <oorr90@gmail.com>
2018-06-29 12:05:25 -0400

    updated featured images with repeater

--- partials/acf-featured.php
+++ partials/acf-featured.php
@@ -10,7 +10,11 @@
 	 	// loop through the rows of data
 	    while ( have_rows($highlights) ) : the_row(); ?>
 
+	    	<?php $image = get_sub_field('home_feature_image'); ?>
+
 	    	<div class="single-highlight">
+
+	    		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
 
 	        	<h2><?php the_sub_field('home_feature_title'); ?></h2>
 
